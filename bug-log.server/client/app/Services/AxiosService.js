eff9fbcb6909445043b838cb4dc3142038932b0f
boriswart <boriswart@yahoo.com>
2021-07-02 16:39:35 -0600

    production build

--- bug-log.server/client/app/Services/AxiosService.js
+++
@@ -1,9 +0,0 @@
-import { baseURL } from '../env.js'
-
-// @ts-ignore
-// eslint-disable-next-line no-undef
-export const api = axios.create({
-  baseURL: baseURL,
-  timeout: 8000,
-  withCredentials: true
-})
