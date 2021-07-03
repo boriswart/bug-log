7a3b89e770668a4ec26306e36706f65b038de631
boriswart <boriswart@yahoo.com>
2021-07-03 16:47:59 -0600

    added about message.

--- bug-log.client/src/pages/AboutPage.vue
+++ bug-log.client/src/pages/AboutPage.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="about">
-    <h1>This is the about page</h1>
+    <h1>William Andersen</h1>
+    <p> This bug-log web application was reated for Codeworks check. It demonstrates my</p>
+    <p> ability to code in Node.js Vue.js and the backend was created with Vue.js </p>
+    <br>
+    <p> The web Application was deployed on heroku.. </p>
+    <p> More later ....</p>
   </div>
 </template>
 
